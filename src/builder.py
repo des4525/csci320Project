eb67f2450abe077cb1b24cd015f98ba87c02cbe5
Jonathan Pyc <jtp8167@rit.edu>
2021-04-01 22:57:58 -0400

    initial commit of builder.py

---
+++ src/builder.py
@@ -0,0 +1,13 @@
+#
+# Builder.py
+#
+# This file will manage the bulk uploading of data to the database.
+#
+# Author: Jonathan Pyc @ jtp8167@rit.edu
+
+def main():
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
