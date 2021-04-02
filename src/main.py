3a5853a39471477c81cbe850accd066ca5367b77
root <root@DuncansDesktop.localdomain>
2021-04-02 16:23:32 -0400

    Added search functionality for Album Artist and Genre

--- src/main.py
+++ src/main.py
@@ -289,8 +289,8 @@ def song_search():
 
 def artist_search():
 	print("What kind of search would you like?")
-	print(" 0. Exact (Song = search)")
-	print(" 1. Close (Song inclused search)")
+	print(" 0. Exact (Artist = search)")
+	print(" 1. Close (Artist inclused search)")
 	
 	while True:
 		try:
@@ -317,8 +317,8 @@ def artist_search():
 		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
 		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
 		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" LIKE %s
-		ORDER BY "Song"."name";
+		WHERE "Artist"."name" LIKE %s
+		ORDER BY "Artist"."name";
 		'''
 		search = "%" + search + "%"	
 	else:
@@ -331,8 +331,8 @@ def artist_search():
 		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
 		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
 		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" = %s
-		ORDER BY "Song"."name";
+		WHERE "Artist"."name" = %s
+		ORDER BY "Artist"."name";
 		'''
 	cursor = connection.cursor()
 	cursor.execute(sql, (search,))
@@ -354,71 +354,6 @@ def artist_search():
 	cursor.close()
 	
 	
-	def song_search():
-	print("What kind of search would you like?")
-	print(" 0. Exact (Song = search)")
-	print(" 1. Close (Song inclused search)")
-	
-	while True:
-		try:
-                	choice = int(input("Enter option #: "))
-                except ValueError:
-			print("Please only enter numbers")
-			choice = -1
-			continue
-		if choice == 0 or choice == 1:
-                        break
-                else:
-                    print("Please choose an option...")
-
-	search = raw_input("Enter your search parameter: ").strip()
-	
-
-	if choice:
-		sql = ''' 
-		SELECT "Song"."name", "Album"."name", "Artist"."name", "Genre"."name", "Song"."length", "Song"."listens"
-		FROM (((((("Song"
-		INNER JOIN "AlbumContains" ON "AlbumContains"."songid" = "Song"."songid")
-		INNER JOIN "Album" ON "Album"."albumid" = "AlbumContains"."albumid")
-		INNER JOIN "ArtistReleases" ON "ArtistReleases"."songid" = "Song"."songid")
-		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
-		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
-		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" LIKE %s
-		ORDER BY "Song"."name";
-		'''
-		search = "%" + search + "%"	
-	else:
-		sql = ''' 
-		SELECT "Song"."name", "Album"."name", "Artist"."name", "Genre"."name", "Song"."length", "Song"."listens"
-		FROM (((((("Song"
-		INNER JOIN "AlbumContains" ON "AlbumContains"."songid" = "Song"."songid")
-		INNER JOIN "Album" ON "Album"."albumid" = "AlbumContains"."albumid")
-		INNER JOIN "ArtistReleases" ON "ArtistReleases"."songid" = "Song"."songid")
-		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
-		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
-		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" = %s
-		ORDER BY "Song"."name";
-		'''
-	cursor = connection.cursor()
-	cursor.execute(sql, (search,))
-	result = cursor.fetchall()
-	
-	
-	if result != []:
-		for entry in result:
-			print "Song: " + entry[0]
-			print "Album: " + entry[1]
-			print "Artist: " + entry[2]
-			print "Genre: " + entry[3]
-			print "Length: " + str(entry[4]) + " Seconds"
-			print "Listen Count: " + str(entry[5]) + " Play(s)"
-			print '\n'	
-	else:
-		print "Sorry but there were no records that matched your search"
-
-	cursor.close()
 	
 def album_search():
 	print("What kind of search would you like?")
@@ -450,8 +385,8 @@ def album_search():
 		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
 		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
 		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" LIKE %s
-		ORDER BY "Song"."name";
+		WHERE "Album"."name" LIKE %s
+		ORDER BY "Album"."name";
 		'''
 		search = "%" + search + "%"	
 	else:
@@ -464,8 +399,8 @@ def album_search():
 		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
 		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
 		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" = %s
-		ORDER BY "Song"."name";
+		WHERE "Album"."name" = %s
+		ORDER BY "Album"."name";
 		'''
 	cursor = connection.cursor()
 	cursor.execute(sql, (search,))
@@ -517,8 +452,8 @@ def genre_search():
 		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
 		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
 		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" LIKE %s
-		ORDER BY "Song"."name";
+		WHERE "Genre"."name" LIKE %s
+		ORDER BY "Genre"."name";
 		'''
 		search = "%" + search + "%"	
 	else:
@@ -531,8 +466,8 @@ def genre_search():
 		INNER JOIN "Artist" ON "Artist"."name" = "ArtistReleases"."aname")
 		INNER JOIN "GenreClassifies" ON "GenreClassifies"."songid" = "Song"."songid")
 		INNER JOIN "Genre" ON "Genre"."name" = "GenreClassifies"."gname")
-		WHERE "Song"."name" = %s
-		ORDER BY "Song"."name";
+		WHERE "Genre"."name" = %s
+		ORDER BY "Genre"."name";
 		'''
 	cursor = connection.cursor()
 	cursor.execute(sql, (search,))
