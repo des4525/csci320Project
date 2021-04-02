37fbe4a720a0a2c4c9080d6cf89500a4cfdca444
root <root@DuncansDesktop.localdomain>
2021-04-02 01:42:54 -0400

    Got the db connection working and register_user() works

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-#This will be the main file that handles the menu and general functionality
-
+#	main.py
+#
+#	This will be the main file that handles the menu and general functionality
+#
+#	Author: Duncan Small
 import os
 from time import sleep
 from datetime import datetime, timedelta
@@ -8,14 +11,15 @@ import psycopg2
 
 
 def main():
-        usr = "p320_02c"
-        pw = "40lmwVV8ftOn"
+	usr = "p320_02c"
+	pw = "40lmwVV8ftOn"
 
 	global connection
 
-	connection = psycopg2.connect("dbname=" + usr + " user=" + usr + " password=" + pw + " host=reddwarf.cs.rit.edu")
+	connection = psycopg2.connect("dbname=" + usr+ " user=" + usr + " password=" + pw + " host=reddwarf.cs.rit.edu")
+
 	print("Connected with: " + connection.dsn)
-        start()
+	start()
 	
 	connection.close()    
     
@@ -23,6 +27,7 @@ def main():
 def start():
         while True:
                 show_main_menu()
+		#TODO put in try catch 
                 choice = int(input("Enter option #: "))
                 if choice == 0:
                         print("Thanks for using our tool.")
@@ -44,11 +49,83 @@ def start():
 def register_user():
 	cursor = connection.cursor()
 
-	query = "SELECT username FROM user;"
-	cursor.execute(query)
+	queryUsername = 'SELECT "username" FROM "User";'
+	cursor.execute(queryUsername)
 	usernames = cursor.fetchall()
+
+	isUsernameValid = False
 	
+	usr =""	
+	while not isUsernameValid:
+		usr = str(raw_input("Enter your desired username: "))
+		usr = usr.strip()
+		isUsernameValid = True
+		if len(usr) == 0:
+			isUsernameValid = False
+			print("Please enter an actual username (Must contain characters)!")
+			continue
+		for name in usernames:
+			print(name[0])
+			if name[0] == usr:
+				print("That name is already taken, please try again!")
+				isUsernameValid = False
+				break
+		
 	
+	firstName = ""
+	lastName = ""
+	
+	while len(firstName) == 0:
+		firstName = str(raw_input("Enter your first name: ")).strip()
+	
+	while len(lastName) == 0:
+		lastName = str(raw_input("Enter your last name: ")).strip()
+
+
+	queryEmail = 'SELECT "email" FROM "User";'
+	cursor.execute(queryEmail)
+	emails = cursor.fetchall()
+
+	isEmailValid = False
+	
+	while not isEmailValid:
+		userEmail = raw_input("Enter your email: ").strip()
+		isEmailValid = True
+		if len(userEmail) == 0: #TODO make sure its actually an email
+			print("Please enter a valid email!")
+			isEmailValid = False
+			continue
+		for email in emails:
+			print(email[0])
+			if userEmail == email[0]:
+				isEmailValid = False
+				print("That email is already taken, be sure you don't already have an account!")
+				break
+		
+	
+	now = datetime.now()
+	creation_time = now.strftime("%Y/%m/%d")
+	registerQuery = 'INSERT INTO "User" ("email", "username", "fname", "lname", "cdate", "ladate") VALUES (%s, %s, %s, %s, %s, %s);'
+	
+	cursor.execute(registerQuery, (userEmail, usr, firstName, lastName, creation_time, creation_time))
+	connection.commit()
+	check = 'SELECT "username" FROM "User" where "username"=%s;' 
+	cursor.execute(check, (usr,))
+	result = cursor.fetchall()
+	
+	i = 0
+	for users in result:
+		i+= 1
+	
+	if i == 0:
+		print("There might have been an issue registering you, please try again or query the User table.")
+	elif i == 1:
+		print("You have been successfully registered, welcome!")
+	else:
+		print("THERE HAS BEEN A GRAVE ERROR PLEASE CHECK THE TABLES FOR INCONSISTENCY")
+		exit(-1)
+	
+	cursor.close()
 
 def show_main_menu():
         """
